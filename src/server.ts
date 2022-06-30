86f23943492da4c50748efc2b17e000915d06c31
joaopaulo <joaopaulosg17@gmail.com>
2022-06-30 18:53:34 -0300

    Adicionado cors como dependencia

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { postRouter } from "./routes/postRoutes";
 import { userRouter } from "./routes/userRoutes";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/users",userRouter);
 app.use("/posts",postRouter);
 
